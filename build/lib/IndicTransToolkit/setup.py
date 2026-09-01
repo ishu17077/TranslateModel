9f1524f925ef8bff620a4d3666e8573bc0a1601a
ishu17077 <ishu17077@users.noreply.github.com>
2026-09-01 22:55:49 +0530

    Add: onnx model to be added in nllb folder

---
+++ build/lib/IndicTransToolkit/setup.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:89da24af76d9d8bc2473ee72334dc26550e3d3f00ae088bde1c70162f48a6045
+size 603
---
+++ build/lib/indictrans2_model/configuration_indictrans.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d7e4ce84efacc18981228f5071b00dba6e3bf165f8ea7bcc8ecdada8942e1a52
+size 14155
